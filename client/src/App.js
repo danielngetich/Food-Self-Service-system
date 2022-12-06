72b156139323d5d8c3e9aa2c5687a787eda010dc
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-12-06 08:41:47 +0300

    add styles to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Menu from './components/Menu';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Menu />
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,17 @@
+import "./Home.css";
+function Home() {
+  return (
+    <div>
+      <div>
+        <h1>WELCOME TO FOODIES RESTAURANT</h1>
+        <h2>The Home of African Delicacies</h2>
+      </div>
+      <div>
+        <h2>Choose your Preferance</h2>
+        <button>Take Away</button>
+        <button>Eat In</button>
+      </div>
+    </div>
+  );
+}
+export default Home;
