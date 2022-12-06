3803c9b7c732c20193ad4b55792cf243360494ba
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-12-06 13:00:34 +0300

    add addFood style

--- client/src/components/AddFood.js
+++ client/src/components/AddFood.js
@@ -1,29 +1,38 @@
-import "./AddFood.css"
-function AddFood(){
-    return(
-        <div className="testbox">
-            <form>
-                <div>
-                    <label>Food Name</label>
-                    <input type="text" placeholder="Enter food Name"></input>
-                </div>
-                <div>
-                    <label>Image Url</label>
-                    <input type="text" placeholder="Enter Image Url"></input>
-                </div>
-                <div>
-                    <label>Food description</label>
-                    <textarea type="text" placeholder="Enter Food description"></textarea>
-                </div>
-                <div>
-                    <label>Price</label>
-                    <input type="integer" placeholder="Enter Price"></input>
-                </div>
-                <div>
-                    <button>Post Food</button>
-                </div>
-            </form>
+import "./AddFood.css";
+function AddFood() {
+  return (
+    <div className="testbox">
+      <form>
+        <div className="category">
+          <label>Category</label>
+          <select>
+            <option>Alcoholic Drinks</option>
+            <option>Hot Drinks</option>
+            <option>Soft Drinks</option>
+            <option>Main Dishes</option>
+          </select>
         </div>
-    )
+        <div>
+          <label>Food Name</label>
+          <input type="text" placeholder="Enter food Name"></input>
+        </div>
+        <div>
+          <label>Image Url</label>
+          <input type="text" placeholder="Enter Image Url"></input>
+        </div>
+        <div>
+          <label>Food description</label>
+          <textarea type="text" placeholder="Enter Food description"></textarea>
+        </div>
+        <div>
+          <label>Price</label>
+          <input type="integer" placeholder="Enter Price"></input>
+        </div>
+        <div>
+          <button>Post Food</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 export default AddFood;
